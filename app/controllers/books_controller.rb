b7ed9059e1087bdb3a67fd5db81f9a12cf26d9b3
norikkun <161901725+norikkun@users.noreply.github.com>
2024-04-20 15:51:08 +0000

    [finish] 応用課題３終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @books = Book.new
     @user = @book.user
+    @post_comment = PostComment.new
   end
 
   def index
